cfe56c42d36388f5c9df75370191d6c3a90750cc
Vivi-anne <viviannekwamboka98@gmail.com>
2022-12-12 07:03:03 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print digits of base 10
+ * Return: zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
