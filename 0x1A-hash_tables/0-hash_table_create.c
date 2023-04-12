8daee8e19f333c03c385353b495a33e83368666f
Vivi-anne <viviannekwamboka98@gmail.com>
2023-04-12 16:30:22 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (!ht->array)
+	if (ht->array == NULL)
 		return (NULL);
 	for (a = 0; a < size; a++)
 		ht->array[a] = NULL;
