1defa957ddd290aa3d38ad705be983c9d4ce2759
Vivi-anne <viviannekwamboka98@gmail.com>
2023-04-12 16:39:54 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,10 +2,10 @@
 
 /**
  * hash_table_create - Creates a hash table.
- * @size: size of array.
+ * @size: size of the array.
  *
  * Return: If error occurs - NULL.
- * Otherwise - pointer to the new hash table.
+ * Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
