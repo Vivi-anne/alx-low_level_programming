ab9c96553d801f5d13733e273a8844f079dd42e6
Vivi-anne <viviannekwamboka98@gmail.com>
2023-07-12 14:14:01 -0700

    advanced

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,70 @@
+#include "search_algos.h"
+
+/**
+  * _binary_search - Searches for value in sorted array
+  *                  of integers with binary search.
+  * @array: Pointer to the first element of array to search.
+  * @left: Starting index of [sub]array to search.
+  * @right: Ending index of [sub]array to search.
+  * @value: Value to search for.
+  *
+  * Return: If the value is not present or array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints[sub]array being searched after each change.
+  */
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (x = left; x < right; x++)
+			printf("%d, ", array[x]);
+		printf("%d\n", array[x]);
+
+		x = left + (right - left) / 2;
+		if (array[x] == value)
+			return (x);
+		if (array[x] > value)
+			right = x - 1;
+		else
+			left = x + 1;
+	}
+
+	return (-1);
+}
+
+/**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: Pointer to the first element of the array to search.
+  * @size: Number of elements in the array.
+  * @value: Value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t x = 0, right;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (x = 1; x < size && array[x] <= value; x = x * 2)
+			printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+	}
+
+	right = x < size ? x : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", x / 2, right);
+	return (_binary_search(array, x / 2, right, value));
+}
